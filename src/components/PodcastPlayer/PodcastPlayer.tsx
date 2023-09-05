5af3aa518e439a01eba499272ba6e453f044935e
YogeshBakshi <yogeshbakshi589@gmail.com>
2023-09-05 17:43:28 +0530

    podcast components style changed.

--- src/components/PodcastPlayer/PodcastPlayer.tsx
+++ src/components/PodcastPlayer/PodcastPlayer.tsx
@@ -1,14 +1,62 @@
-import { Grid, Typography } from '@mui/material'
+import { Card, CardContent, CardMedia, Grid, Link, Typography } from '@mui/material'
 import { FC } from 'react'
+import { addPodData } from '../../podslice/slice'
 
 interface dataPod{
     dataPodcast:any
 }
 const Podcastplayer:FC<dataPod> = ({dataPodcast}) => {
+    const backGround='../../../public/background.jpg'
   return (
-    <Grid>
-     <Grid item><Typography variant='h1'>NOne</Typography></Grid>
+   <>
+     
+     <Grid container
+        sx={{
+          width: "100vw",
+          height: "50vh",
+          backgroundImage:`url(${backGround})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          WebkitBackgroundSize:'cover',
+          mt:2
+        }}/>
+        <Grid  container sx={{display:'flex',width:'100vw',background:'#2D2D2D'}}>
+     <Grid item sx={{borderRadius:'4px',display:'flex',maxWidth:'40vw',justifyContent:'flex-start',flexWrap:'wrap'}} >
+    {dataPodcast.map((item:any)=>{
+        return(
+            <>
+                <Card sx={{ Width:200,background:'#1F1E1E',m:3}} key={item.id}>
+                <CardMedia
+        component="img"
+        height="194"
+        image={item.imgSRC}
+        alt="Paella dish"
+        sx={{"&:hover": {
+          transform: 'scale(1.1)',
+        }}}
+      />
+      <CardContent>
+        <Typography variant="body2" color="#F8F8F8">
+          {item.title}
+        </Typography>
+      </CardContent>
+                </Card>
+            </>
+        )
+      })}
+      </Grid>
+      <Grid sx={{width:'70vw'}}>{dataPodcast.map((item:any)=>{
+        return(
+          <>
+       <Typography variant='h3' sx={{color:'white'}}>{item.title}</Typography>
+       <Typography variant='h6' sx={{color:'white'}}>{item.audioLength}</Typography>
+       <Typography component={'div'} sx={{color:'white',fontSize:'800',lineHeight:2}}>{item.description}</Typography>
+       </>
+        )
+      })}</Grid>
     </Grid>
+    </>
   )
 }
 
