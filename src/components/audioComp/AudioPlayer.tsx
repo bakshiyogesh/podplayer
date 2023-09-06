da3c1d179dc8cdf8d4c252a73962b418ccd60f72
YogeshBakshi <yogeshbakshi589@gmail.com>
2023-09-06 10:55:34 +0530

    volume buttons set accordingly and started working.

--- src/components/audioComp/AudioPlayer.tsx
+++ src/components/audioComp/AudioPlayer.tsx
@@ -3,20 +3,20 @@ import Slider from "@mui/material/Slider";
 import IconButton from "@mui/material/IconButton";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import PauseIcon from "@mui/icons-material/Pause";
-import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
 import VolumeOffIcon from "@mui/icons-material/VolumeOff";
-import Forward10Icon from '@mui/icons-material/Forward10';
+import Forward10Icon from "@mui/icons-material/Forward10";
 import { Grid } from "@mui/material";
-interface AudioProps{
-    selectData:any
+interface AudioProps {
+  selectData: any;
 }
 
-const AudioPlayer:FC<AudioProps> = ({selectData}) => {
-    console.log("selectedData",selectData);
-    
-    const audioRef = useRef<HTMLAudioElement>(null);
+const AudioPlayer: FC<AudioProps> = ({ selectData }) => {
+  console.log("selectedData", selectData);
+
+  const audioRef = useRef<HTMLAudioElement>(null);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [volume, setVolume] = useState(false);
+  const [muted, setMuted] = useState(false);
   const [duration, setDuration] = useState(0);
   const [currentTime, setCurrentTime] = useState(0);
   const togglePlay = () => {
@@ -33,22 +33,22 @@ const AudioPlayer:FC<AudioProps> = ({selectData}) => {
     setDuration(audioRef.current!.duration);
   };
 
-  const handleSeek = (event:any, newValue:any) => {
+  const handleSeek = (event: any, newValue: any) => {
     const newTime = (newValue / 100) * duration;
-    console.log("duration:",duration);
-    
-    console.log("newTime:",newTime)
+    console.log("duration:", duration);
+
+    console.log("newTime:", newTime);
     audioRef.current!.currentTime = newTime;
     setCurrentTime(newTime);
   };
 
-  const handleVolumeChange = (event:any, newValue:any) => {
-    audioRef.current!.volume = newValue / 100;
-    setVolume(newValue);
+  const handlemutedChange = (event: any,) => {
+    audioRef.current!.muted = !audioRef.current?.muted;
+    setMuted(!muted);
   };
   return (
-    <Grid container sx={{background:'#454545'}}>
-        <audio
+    <Grid container sx={{ background: "#454545" }}>
+      <audio
         ref={audioRef}
         src={selectData[0].audioURL}
         onTimeUpdate={handleTimeUpdate}
@@ -61,16 +61,16 @@ const AudioPlayer:FC<AudioProps> = ({selectData}) => {
         onChange={handleSeek}
         min={0}
         max={100}
-        aria-labelledby="volume-slider"
+        aria-labelledby="muted-slidMr"
         style={{ width: "320px" }}
-        sx={{m:2}}
+        sx={{ m: 2 }}
       />
-      <IconButton onClick={() => setVolume(!volume)}>
-        {volume ? <VolumeOffIcon /> : <VolumeUpIcon />}
+      <IconButton onClick={handlemutedChange}>
+        {muted ?<VolumeOffIcon />:<VolumeUpIcon/>}
       </IconButton>
-      <Forward10Icon sx={{m:2}}/>
+      <Forward10Icon sx={{ m: 2 }} />
     </Grid>
-  )
-}
+  );
+};
 
 export default AudioPlayer;
