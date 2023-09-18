34e89d195a6a726aaa23c8558a891ad436cb349a
YogeshBakshi <yogeshbakshi589@gmail.com>
2023-09-18 11:03:54 +0530

    path chnages for files.

--- src/constant/Data.ts
+++ src/constant/Data.ts
@@ -15,7 +15,7 @@ const CONST_POD_DATA :podData[]= [
     audioURL:
       "https://dcs.megaphone.fm/MILAR8291884907.mp3",
     title: "Millionaire University",
-    imgSRC:'/public/milionaire_university.jpeg',
+    imgSRC:'/milionaire_university.jpeg',
     description: "It is our belief that ANYONE can start and grow a successful business and we've created Millionaire University to help you get there! At Millionaire University, we follow a proven process that has been used to create successful businesses time and time again. Do you want to start a business or currently have a business you want to grow? Do you have a skill you want to turn into a business? Do you want to learn from other successful business owners and connect to like-minded entrepreneurs? Do you believe there is a better way to learn and a better way to create wealth? Then welcome to Millionaire University. Your school for wealth creation. Graduate rich, not broke. To get your FREE Business Training, go to MillionaireUniversity.com/training.",
    audioLength:'30:55'
   }
@@ -26,7 +26,7 @@ const CONST_POD_DATA :podData[]= [
       "https://audio.transistor.fm/m/shows/41076/dai/ea8f1e29d199d9db3eb3fe518b982ea5.mp3",
     title:
       "Zero to Unicorn",
-    imgSRC:'/public/zero_unicorn.jpeg',
+    imgSRC:'/zero_unicorn.jpeg',
     description: "A unicorn is a magical creature shrouded in mystery known for its ability to make the impossible possible. In modern times, the word “unicorn” has come to mean something intrinsically valuable and difficult to obtain, whether that be a business with a $1 billion dollar valuation, or the love of a special someone. On Zero to Unicorn, we’ll hear directly from the Kaeding family as they recount their history of founding and operating Norhart, a unicorn-level business in the construction industry with a mission to positively impact the way people live. But this isn’t a story of how to grow a business to a billion-dollar valuation. This tale is centered around a special group of people who are coming together to create something bigger than themselves. Despite several trials, the people of Norhart seek to make a positive impact on the world—and, ultimately, to do what every unicorn is born to do: make the impossible possible. Join us weekly for this limited series chronicling the Kaeding family’s journey, from starting a family-owned business to becoming a unicorn with a business determined to do good.",
     audioLength:'38:38'
   },
@@ -35,7 +35,7 @@ const CONST_POD_DATA :podData[]= [
     audioURL:
       "https://d3ctxlq1ktw2nl.cloudfront.net/staging/2023-7-28/344804762-44100-2-7023f5302d1ef.mp3",
     title: "The Influence Factor ",
-    imgSRC:'/public/influence_factor.jpeg',
+    imgSRC:'/influence_factor.jpeg',
     description: "Alessandro Bogliari, CEO and Co-Founder of The Influencer Marketing Factory, a global influencer marketing agency, talks with great guests about influencer marketing, social media, the creator economy, social commerce and much more.",
    audioLength:'48:39'
   },
@@ -44,7 +44,7 @@ const CONST_POD_DATA :podData[]= [
     audioURL:
       "https://dcs.megaphone.fm/HS2090760096.mp3",
     title: "My First Million",    
-    imgSRC:'/public/my_first_million.jpeg',
+    imgSRC:'/my_first_million.jpeg',
     description:
       "Sam Parr and Shaan Puri brainstorm new business ideas based on trends & opportunities they see in the market. Sometimes they bring on famous guests to brainstorm with them.",
    audioLength:'1:49:43'
@@ -54,7 +54,7 @@ const CONST_POD_DATA :podData[]= [
     audioURL:
       "https://s342.podbean.com/pb/1028c8e3cff27689605ea25bfeb15fb9/64f7990b/data1/fs156/4944510/uploads/JJD-Ep125-Audio.mp3",
     title: "Jiu Jitsu Dummies",
-    imgSRC:'/public/jiu_jitsu_dummies.jpeg',
+    imgSRC:'/jiu_jitsu_dummies.jpeg',
     description:
       "This week we are joined by 2nd degree Black Belt Malachy Friedman. Malachy received his Black Belt from jiu-jitsu legend Ricardo Liborio. Most of us have gotten to know Malachy from his Instagram Reels and online instructionals but there's much more here to unpack with this former American Top Team member. Check out the episode and let us know what you think",
   audioLength:'1:40:40'
@@ -65,7 +65,7 @@ const CONST_POD_DATA :podData[]= [
       "https://dcs.megaphone.fm/TPG3415315545.mp3",
     title:
       "Gravy",
-    imgSRC:"/public/gravy.jpeg",
+    imgSRC:"/gravy.jpeg",
     description: "Gravy shares stories of the changing American South through the foods we eat. Gravy showcases a South that is constantly evolving, accommodating new immigrants, adopting new traditions, and lovingly maintaining old ones. It uses food as a means to explore all of that, to dig into lesser-known corners of the region, complicate stereotypes, document new dynamics, and give voice to the unsung folk who grow, cook, and serve our daily meals.",
     audioLength:'25:28'
   },
@@ -75,7 +75,7 @@ const CONST_POD_DATA :podData[]= [
       "https://dcs.megaphone.fm/MERE3258045955.mp3",
     title:
       "The Better Buy",
-    imgSRC:'/public/the_better_buy.jpeg',
+    imgSRC:'/the_better_buy.jpeg',
     description: "The Better Buy, a podcast from Better Homes & Gardens is back for Season 2! This season we'll delve into the full home buying process, from house hunting and moving to interior design and landscaping. Each episode will progress through this homeownership journey, with host Mélanie Berliet talking to both experts and celebrities who will share their stories, practical tips, and advice. We’re on a mission to inspire and empower you to create your dream home. New episodes every Wednesday!",
    audioLength:'28:28'
   },
@@ -85,7 +85,7 @@ const CONST_POD_DATA :podData[]= [
       "https://dcs.megaphone.fm/HS7044766323.mp3",
     title:
       "Marketing Against The Grain",
-    imgSRC:"/public/marketing_against.jpeg",
+    imgSRC:"/marketing_against.jpeg",
     description: "Kipp Bodnar (HubSpot’s CMO) and Kieran Flanagan (Zapier’s CMO), lead you down the rabbit hole of marketing trends, growth tactics and innovation. On the way you’ll pick up undiscovered strategies to give you that slight edge for success. These are not your typical twitter thread regurgitated marketing tactics that everyone is doing. These are new methods, with unfiltered examination of successful fresh ideas",
    audioLength:'33:02'
   },
@@ -102,7 +102,7 @@ const CONST_POD_DATA :podData[]= [
     id:11,
     audioURL:'https://www.podtrac.com/pts/redirect.mp3/pdst.fm/e/chrt.fm/track/G481GD/traffic.megaphone.fm/ADV8318757959.mp3',
     title:'TEAM EXECUTER',
-    imgSRC:"/public/burningflag.jpeg",
+    imgSRC:"/burningflag.jpeg",
     description:'Team Xecuter was a group involved with making and selling modchips for video game systems. They often made mods that allowed the video game system to rip games or play pirated games. It was a crowd favorite in the modding scene. Until it all fell apart. The story of what happened to Team Xecuter must be heard to believe.',
     audioLength:'1:28:00'
   }
